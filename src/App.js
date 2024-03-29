033c8389b11d71b8178e0fc6df18a6a835d8f14a
Aniket Kumar <kraniket123654@gmail.com>
2024-03-29 16:57:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   let logoItemCont = useRef(null);
 
   const handleReload1 = () => {
-    TweenMax.to(logoItem1, 3, { css: { top: "40vh", right: "40vw", opacity: "1", transform: "scale(2) rotateZ(0)" } })
+    TweenMax.to(logoItem1, 3, { css: { top: "40vh", right: "45vw", opacity: "1", transform: "scale(2) rotateZ(0)" } })
   }
   const handleClick1 = () => {
     TweenMax.to(logoItemCont, 0, { css: { position: "relative" } })
