4e8ee98fa389909225fe0c150d51f5b226a4e485
Aniket Kumar <kraniket123654@gmail.com>
2024-03-29 16:46:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+import { TweenMax } from 'gsap';
+
+const App = () => {
+  let logoItem1 = useRef(null);
+  let logoItemCont = useRef(null);
+
+  const handleReload1 = () => {
+    TweenMax.to(logoItem1, 3, { css: { top: "50vh", right: "50vw", opacity: "1", transform: "scale(3) rotateZ(0)" } })
+  }
+  const handleClick1 = () => {
+    TweenMax.to(logoItemCont, 0, { css: { position: "relative" } })
+    TweenMax.to(logoItem1, 3, { css: { top: "0vh", right: "0vw", opacity: "1", transform: "scale(1)" } })
+  }
+
+  useEffect(handleReload1,[]);
+
+  function getRandomPair() {
+    const randomValue = Math.random();
+    let top, right;
+
+    if (randomValue < 0.25) {
+      top = 0;
+      right = Math.floor(Math.random() * 101);
+    } else if (randomValue < 0.5) {
+      right = 0;
+      top = Math.floor(Math.random() * 101);
+    } else if (randomValue < 0.75) {
+      top = 105;
+      right = Math.floor(Math.random() * 101);
+    } else {
+      right = 105;
+      top = Math.floor(Math.random() * 101);
+    }
+
+    return { top, right };
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", justifyContent: "center", height: "100vh", alignItems: "center" }}>
+      <div className='divu-container' ref={e => logoItemCont = e}>
+        <div style={{ position: "absolute", top: `${getRandomPair().top}vh`, right: `${getRandomPair().right}vw` }} className='divu1' ref={e => logoItem1 = e} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
